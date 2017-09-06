cc08d72f253f0e955bd599cc738ce36edc515994
Keegan Ruebling and Margaret Berry <margaret@>
2017-09-06 13:15:58 -0700

    add specs for false statements

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/ ruby
-puts "Player 1 select Rock, Paper, or Scissors"
-player1 = gets.chomp
-puts "Player 2 select Rock, Paper, or Scissors"
-player2 = gets.chomp
+# puts "Player 1 select Rock, Paper, or Scissors"
+# player1 = gets.chomp
+# puts "Player 2 select Rock, Paper, or Scissors"
+# player2 = gets.chomp
 
 class RPS
-  def initialize
-    def wins?(player1, player2)
-      if (player1 === "rock") & (player2 === "scissors")
-        true
-      elsif (player1 === "paper") & (player2 === "rock")
-        true
-      elsif (player1 === "scissors") & (player2 === "paper")
-        true
-      end
+  def wins?(player1, player2)
+    if (player1 === "rock") & (player2 === "scissors")
+      true
+    elsif (player1 === "paper") & (player2 === "rock")
+      true
+    elsif (player1 === "scissors") & (player2 === "paper")
+      true
+    elsif (player1 === "rock") & (player2 === "paper")
+      false
+    elsif (player1 === "scissors") & (player2 === "rock")
+      false
+    elsif (player1 === "paper") & (player2 === "scissors")
+      false
+    elsif (player1 === player2)
+      false
     end
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -1,5 +1,5 @@
 require('rspec')
-# require('pry')
+require('pry')
 require('rock_paper_scissors')
 
   describe('RPS#wins?') do
@@ -16,6 +16,26 @@ require('rock_paper_scissors')
   it("returns true if paper is the first argument and rock is the second") do
     game = RPS.new()
     expect(game.wins?("paper", "rock")).to(eq(true))
+  end
+
+  it("returns false if rock is the first argument and paper is the second") do
+    game = RPS.new()
+    expect(game.wins?("rock", "paper")).to(eq(false))
+  end
+
+  it("returns false if scissors is the first argument and rock is the second") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "rock")).to(eq(false))
+  end
+
+  it("returns false if paper is the first argument and scissors is the second") do
+    game = RPS.new()
+    expect(game.wins?("paper", "scissors")).to(eq(false))
+  end
+
+  it("returns false if both players select the same move") do
+    game = RPS.new()
+    expect(game.wins?("paper", "paper")).to(eq(false))
   end
 
 end
